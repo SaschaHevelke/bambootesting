619be530851cdc1b4a166b4e113ce843578fa098
hevelke <Sascha@Saschas-MacBook-Pro.local>
2017-02-02 22:04:38 +0100

    added hello world project

---
+++ HelloWorld/src/main/java/de/sasa/HelloWorld/App.java
@@ -0,0 +1,13 @@
+package de.sasa.HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
